57bed177e90a2ab921885ae3dc5b78e4f48f2230
Michaell Gomez <maicol_sgf@hotmail.com>
2020-09-26 15:58:22 -0500

    agregando base de proyecto

---
+++ src/components/section/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default function NavBar() {
+    return (
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+}
+
+
